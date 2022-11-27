cdcaf241fe6783e23154cf909fa07d138815851b
ana0101 <74956688+ana0101@users.noreply.github.com>
2022-11-27 13:27:59 +0200

    Add files via upload

---
+++ Ghicitor.py
@@ -0,0 +1,164 @@
+import os
+import math
+
+def initializare():
+    # punem cuvintele in lista cuvinte
+    global cuvinte
+    cuvinte = open("cuvinte.txt", "r")
+    cuvinte = cuvinte.readlines()
+    for i in range(len(cuvinte)):
+        cuvinte[i] = cuvinte[i].strip()
+
+
+def initializareFrecv():
+    global frecvCuvinte
+    # frecvCuvinte este un dictionar cu toate cuvintele, ce are ca valori dictionare cu toate cele 3^5 posibile feedback-uri
+    # si de cate ori apare fiecare feedback pentru un anumit cuvant, raportat la toate celelalte cuvinte din lista
+    frecvCuvinte = {}
+    global cuvinte
+    n = len(cuvinte)
+    for i in range(n):
+        # facem un dictionar cu 3^5 elemente, anume toate feedback-urile posibile
+        frecv = {}
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    for m in range(3):
+                        for o in range(3):
+                            frecv[str(j) + str(k) + str(l) + str(m) + str(o)] = 0
+        frecvCuvinte[cuvinte[i]] = frecv
+
+        # calculam frecventa posibilelor feedback-uri
+        for j in range(n):
+            rel = ""
+            for k in range(5):
+                if cuvinte[i][k] == cuvinte[j][k]:
+                    rel += "2"
+                else:
+                    if cuvinte[i][k] in cuvinte[j]:
+                        rel += "1"
+                    else:
+                        rel += "0"
+            frecv[rel] += 1
+
+
+def incercare():
+    global frecvCuvinte
+    entropieMax = 0
+    global cuvOptim
+    global cuvinte
+    n = len(cuvinte)
+
+    if n > 1:
+        if n != 11454:
+            # calculam entropiile cuvintelor
+            for i in range(n):
+                entropieCuv = 0
+
+                # facem un dictionar cu 3^5 elemente, anume toate feedback-urile posibile
+                frecv = {}
+                for j in range(3):
+                    for k in range(3):
+                        for l in range(3):
+                            for m in range(3):
+                                for o in range(3):
+                                    frecv[str(j) + str(k) + str(l) + str(m) + str(o)] = 0
+
+                # calculam frecventele posibilelor feedback-uri
+                for j in range(n):
+                    rel = ""
+                    for k in range(5):
+                        if cuvinte[i][k] == cuvinte[j][k]:
+                            rel += "2"
+                        else:
+                            if cuvinte[i][k] in cuvinte[j]:
+                                rel += "1"
+                            else:
+                                rel += "0"
+                    frecv[rel] += 1
+
+                # calculam probabilitatile feedback-urilor
+                for j in range(3):
+                    for k in range(3):
+                        for l in range(3):
+                            for m in range(3):
+                                for o in range(3):
+                                    p = frecv[str(j) + str(k) + str(l) + str(m) + str(o)] / n
+                                    if p != 0:
+                                        entropieCuv += p * math.log2(1/p)
+                                        entropieCuv = round(entropieCuv, 10)
+
+                # verficam daca entropia cuvantului este mai mare decat cea maxima curenta
+                if entropieCuv > entropieMax:
+                    entropieMax = entropieCuv
+                    cuvOptim = cuvinte[i]
+
+        else:
+            # calculam entropiile cuvintelor
+            for i in range(n):
+                entropieCuv = 0
+                # calculam probabilitatile feedback-urilor
+                for j in range(3):
+                    for k in range(3):
+                        for l in range(3):
+                            for m in range(3):
+                                for o in range(3):
+                                    p = frecvCuvinte[cuvinte[i]][str(j) + str(k) + str(l) + str(m) + str(o)] / n
+                                    if p != 0:
+                                        entropieCuv += p * math.log2(1 / p)
+                                        entropieCuv = round(entropieCuv, 10)
+
+                # verficam daca entropia cuvantului este mai mare decat cea maxima curenta
+                if entropieCuv > entropieMax:
+                    entropieMax = entropieCuv
+                    cuvOptim = cuvinte[i]
+    else:
+        cuvOptim = cuvinte[0]
+
+    # punem cuvantul optim in cuvantOptim.txt
+    cuvOptim = cuvOptim.strip()
+    output = open("cuvantOptim.txt", "w")
+    output.write(cuvOptim)
+    output.flush()
+    os.fsync(output.fileno())
+
+
+def interpretare():
+    global cuvinte
+    global cuvOptim
+
+    # citim feedbak-ul despre cuvantul pe care l-am trimis in feedback.txt
+    feedback = open("feedback.txt", "r")
+    feedback = (feedback.readlines())
+    feedback = ''.join(feedback)
+
+    if feedback == "Ati ghicit cuvantul":
+        feedback.close()
+    else:
+        cuvinte.remove(cuvOptim)
+
+        # eliminam toate cuvintele din lista de cuvinte care nu se potrivesc cu feedback-ul primit
+        j = 0
+        while j < len(cuvinte):
+            for i in range(5):
+
+                if feedback[i] == '0':
+                    if cuvOptim[i] in cuvinte[j] and cuvinte[j] in cuvinte:
+                        cuvinte.remove(cuvinte[j])
+                        break
+
+                elif feedback[i] == '1':
+                    if cuvOptim[i] not in cuvinte[j] and cuvinte[j] in cuvinte:
+                        cuvinte.remove(cuvinte[j])
+                        break
+
+                    elif cuvOptim[i] == cuvinte[j][i] and cuvinte[j] in cuvinte:
+                        cuvinte.remove(cuvinte[j])
+                        break
+
+                elif feedback[i] == '2':
+                    if cuvOptim[i] != cuvinte[j][i] and cuvinte[j] in cuvinte:
+                        cuvinte.remove(cuvinte[j])
+                        break
+            else:
+                j += 1
---
+++ WordleV2.py
@@ -0,0 +1,66 @@
+from GhicitorV2 import *
+import random
+import os
+
+# deschidem fisierul cu toate cuvintele
+cuvinte = open('cuvinte.txt', 'r')
+cuvinte = cuvinte.readlines()
+
+# deschidem fisierul in care dam feedback despre cuvantul ghicit
+output = open('feedback.txt', 'w')
+n = len(cuvinte)
+
+# deschidem fisierul in care punem solutiile
+solutii = open('solutii.txt', 'w')
+
+for i in range(len(cuvinte)):
+    cuvinte[i] = cuvinte[i].strip()
+
+
+initializare()
+
+
+# alegem cuvantul de ghicit
+cuv = random.choice(cuvinte)
+print("cuvantul de ghicit este",cuv)
+
+
+while True:
+    incercare()
+    # citim incercarea ghicitorului
+    cuvant = open('cuvantOptim.txt', 'r')
+    cuvant = cuvant.readlines()
+    cuvant = ''.join(cuvant)
+
+    print("cuvantul ghicit este", cuvant)
+
+    # comparam incercarea si cuvantul de ghicit, punand informatile intr-un fisier feedback.txt
+    if cuv == cuvant:
+        output.seek(0)
+        output.truncate()
+        output.writelines("Ati ghicit cuvantul")
+        output.flush()
+        os.fsync(output.fileno())
+        break
+    else:
+        result = ['-1', '-1', '-1', '-1', '-1']
+
+        for i in range(5):
+            if cuvant[i] == cuv[i]:
+                result[i] = '2'
+
+        for i in range(5):
+            if cuvant[i] in cuv:
+                if cuvant[i] != cuv[i]:
+                    result[i] = '1'
+            else:
+                result[i] = '0'
+
+        result = "".join(result)
+        output.seek(0)
+        output.truncate()
+        output.writelines(result)
+        output.flush()
+        os.fsync(output.fileno())
+        interpretare()
+
